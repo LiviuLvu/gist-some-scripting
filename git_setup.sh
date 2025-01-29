ecd9e1a69e205cf02f0e964f3f3e508035e09d60
liviulvu <0901966+LiviuLvu@users.noreply.github.com>
2025-01-29 09:46:24 -0800

    script: setup git on a new linux machine

---
+++ git_setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Debuging options:
+# -x: Shows each command before it's executed (enables debugging).
+# -v: Prints shell input lines as they're read, showing verbose output.
+# -e: Exits immediately if a command exits with a non-zero status (error).
+# -u: Treats unset variables as an error when substituting.
+# -o pipefail: Makes a pipeline's return status the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully.
+set -euo pipefail
+
+# Ensure Git is installed
+if ! command -v git &> /dev/null
+then
+	sudo apt install -y git
+fi
+# Verify Git installation
+git --version
+
+# Configure Git wit your user details
+git config --global user.name "your_usernamme"
+git config --global user.email "your@mail.com"
+
+# Generate SSH keys and display the public key
+# -f the file to save the new ssh key. It will be created or will ask for overwrite if exists.
+# -N specifiy ssh key pass or leave empty for none
+ssh-keygen -t ed25519 -C "your_noreply_git_mail@users.noreply.github.com" -f ~/.ssh/github_ed25519 -N ""
+	cat ~/.ssh/github_ed25519.pub
+
+echo "Done!"
